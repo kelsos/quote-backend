a9656dfa1820e7d9123f7626b734533de8d02610
Konstantinos Paparas <kelsos86@gmail.com>
2015-05-26 09:39:37 +0300

    Initial addition of Users in the schema Some initial work on registration and authentication.

--- public/index.php
+++ public/index.php
@@ -1,10 +1,13 @@
 <?php
 
+const PASS_LENGTH = 4;
 require_once '../vendor/autoload.php';
 require_once '../generated-conf/config.php';
 
 use Quote\Quote;
 use Quote\QuoteQuery;
+use Quote\User;
+use Quote\UserQuery;
 use QuoteEnd\Error;
 use QuoteEnd\Helpers;
 use Slim\Slim;
@@ -57,6 +60,84 @@ $app->post('/quote', function() use ($app) {
   ];
 
   $app->response()->setBody(json_encode($result));
+
+});
+
+$app->post("/register", function() use ($app) {
+  $request = $app->request;
+  $username = $request->post("username");
+  $password = $request->post("password");
+
+  if (Helpers::isNullOrEmpty($username) || Helpers::isNullOrEmpty($password)) {
+    $error = new Error();
+    $error->setStatus(400);
+    $error->setDescription("Password / Username can't be empty");
+    $app->halt($error->getStatus(), json_encode($error));
+  }
+
+  if (!filter_var($username, FILTER_VALIDATE_EMAIL)) {
+    $error = new Error();
+    $error->setStatus(400);
+    $error->setDescription("Username must be a valid e-mail");
+    $app->halt($error->getStatus(), json_encode($error));
+  }
+
+  if (strlen($password) < PASS_LENGTH) {
+    $error = new Error();
+    $error->setStatus(400);
+    $error->setDescription("Password must be at least ". PASS_LENGTH . " characters long");
+    $app->halt($error->getStatus(), json_encode($error));
+  }
+
+  $password_hash = password_hash($password, PASSWORD_DEFAULT);
+
+  $user = UserQuery::create()->findByUsername($username);
+
+  if ($user != null) {
+    $error = new Error();
+    $error->setStatus(409);
+    $error->setDescription("User already exists");
+    $app->halt($error->getStatus(), json_encode($error));
+  }
+
+  $user = new User();
+  $user->setUsername($username);
+  $user->setPassword($password_hash);
+  $user->setApproved(false);
+  $user->setAdmin(false);
+  $user->save();
+});
+
+$app->post("/login", function() use ($app) {
+  $request = $app->request;
+  $username = $request->post("username");
+  $password = $request->post("password");
+
+  if (Helpers::isNullOrEmpty($username) || Helpers::isNullOrEmpty($password)) {
+    $error = new Error();
+    $error->setStatus(400);
+    $error->setDescription("Password / Username can't be empty");
+    $app->halt($error->getStatus(), json_encode($error));
+  }
+
+  $user = UserQuery::create()->findByUsername($username)->getFirst();
+
+  if ($user == null) {
+    $error = new Error();
+    $error->setStatus(400);
+    $error->setDescription("Invalid username or password");
+    $app->halt($error->getStatus(), json_encode($error));
+  }
+
+  $password_hash = password_hash($password, PASSWORD_DEFAULT);
+
+  if ($user->getPassword() != $password_hash) {
+    $error = new Error();
+    $error->setStatus(400);
+    $error->setDescription("Invalid username or password");
+    $app->halt($error->getStatus(), json_encode($error));
+  }
+
 
 });
 
